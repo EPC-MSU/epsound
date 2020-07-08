eb9af6167a46a9bf24e1f69e846b02af417bb5fa
mikheev <mikheev@physlab.ru>
2020-07-08 12:57:02 +0300

    Correct version setup.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='epsound',
-      version='0.0.12',
+      version='0.1.0',
       description='Sound player library',
       long_description=long_description,
       long_description_content_type="text/markdown",
